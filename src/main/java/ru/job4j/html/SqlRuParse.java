65ae73ebd87f90fbb1486bf62e9e62f603484b01
i1rr <i1r@tuta.io>
2021-09-03 12:09:45 +1000

    Post details loading.

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -6,16 +6,28 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class SqlRuParse {
+
+    private static void getDescription(String link) throws Exception {
+        Document doc = Jsoup.connect(link).get();
+        Element el = doc.select(".msgBody").get(1);
+        System.out.println(el.text());
+    }
+
     public static void main(String[] args) throws Exception {
-        for (int i = 1; i < 6; i++) {
+        int limiter = 0; //to ignore first 3 topics as they are not the part of the task
+        for (int i = 1; i < 6; i++) { //to go through the pages (i < 6 means up to 5th page)
             Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
             Elements row = doc.select(".postslisttopic");
             for (Element td : row) {
-                Element href = td.child(0);
-                System.out.println(href.attr("href"));
-                System.out.println(href.text());
-                System.out.println(td.parent().child(5).text());
-                System.out.println();
+                limiter++;
+                if (limiter > 3) {
+                    Element href = td.child(0);
+                    System.out.println(href.attr("href"));
+                    System.out.println(href.text());
+                    getDescription(href.attr("href")); //description loader
+                    System.out.println(td.parent().child(5).text());
+                    System.out.println();
+                }
             }
         }
     }
